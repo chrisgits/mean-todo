c6ea93fe08a75b1958db9a747069ccf9c807329d
Chris Mannel <cmannel77@gmail.com>
2016-03-04 21:08:31 -0600

    Setup Mock json files
    Cleaned up app and tested.

--- src/api/index.js
+++ src/api/index.js
@@ -2,12 +2,14 @@
 'use strict';
 
 var express = require('express');
+var todos = require('../../mock/todos.json');
 
 var router = express.Router(); //creates router method for all routes
 
 router.get('/todos', function (req, res) {
-  res.json({todos: []});
+  res.json({todos: todos});
 });
+
 
 // TODO: ADD POST route to create new entries
 // TODO: Add PUT route to update uxisting entries
